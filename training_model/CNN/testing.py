1593f8e2b68711dea122283e4833b527c46cfb97
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-10-18 00:27:51 +0530

    Add visualization for CNN model predictions

--- training_model/CNN/testing.py
+++ training_model/CNN/testing.py
@@ -6,3 +6,20 @@
 import matplotlib.pyplot as plt
 import random
 import numpy as np
+
+class_names = {0: "Non-Tumor", 1: "Non-Viable-Tumor", 2: "Viable"}
+
+sample_indices = random.sample(range(len(X_test)), 5)
+fig, axes = plt.subplots(1, 5, figsize=(15, 5))
+
+for i, idx in enumerate(sample_indices):
+    img = X_test[idx]
+    actual_label = int(y_test[idx])
+    predicted_prob = cnn_model.predict(np.expand_dims(img, axis=0))
+    predicted_label = np.argmax(predicted_prob)
+    axes[i].imshow(img)
+    axes[i].axis('off')
+    axes[i].set_title(f"Pred: {class_names[predicted_label]}\nActual: {class_names[actual_label]}")
+
+plt.tight_layout()
+plt.show()
