0946381aa9b0546f4b4a8e2edb394d92882b9856
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-10-18 00:38:05 +0530

    Update GCNTrain.py

--- training_model/CNN+GCN/GCNTrain.py
+++ training_model/CNN+GCN/GCNTrain.py
@@ -1,3 +1,8 @@
 # =============================================
 # 6.4–6.5 Define and Train Graph Convolutional Network (GCN)
 # =============================================
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from sklearn.model_selection import train_test_split
