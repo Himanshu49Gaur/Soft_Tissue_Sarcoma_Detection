bc2a5f7b0bd234ebcdd689842fab8c6f4dbcb945
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-10-18 00:34:51 +0530

    Update convertgraphtopytorch.py

--- training_model/CNN+GCN/convertgraphtopytorch.py
+++ training_model/CNN+GCN/convertgraphtopytorch.py
@@ -1,3 +1,8 @@
 # =============================================
 # 6.3 Convert NetworkX Graph to PyTorch Geometric Format
 # =============================================
+
+import torch
+from torch_geometric.data import Data
+import numpy as np
+import networkx as nx
