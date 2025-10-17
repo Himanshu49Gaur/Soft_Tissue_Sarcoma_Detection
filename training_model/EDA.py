3c775a4a59775688bab14b01ad9b5ce8e3b1eebc
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-10-18 00:11:36 +0530

    Update EDA.py

--- training_model/EDA.py
+++ training_model/EDA.py
@@ -21,3 +21,22 @@ y_test = np.load(os.path.join(preprocessed_dir, "y_test_tensor.npy"))
 
 # Define class labels
 class_labels = ["Non-Tumor", "Non-Viable-Tumor", "Viable"]
+
+# 3.1 Class Distribution After Preprocessing
+plt.figure(figsize=(8, 5))
+sns.countplot(x=y_train, palette="viridis")
+plt.xticks(ticks=[0, 1, 2], labels=class_labels)
+plt.title("Class Distribution in Training Set After Augmentation")
+plt.xlabel("Tumor Class")
+plt.ylabel("Number of Images")
+plt.show()
+
+# 3.2 Visualizing Preprocessed Images
+fig, axes = plt.subplots(3, 5, figsize=(15, 8))
+fig.suptitle("Sample Augmented Images from Training Set", fontsize=14)
+for i, ax in enumerate(axes.flat):
+    idx = np.random.randint(0, len(X_train))
+    ax.imshow(X_train[idx])
+    ax.set_title(class_labels[y_train[idx]])
+    ax.axis("off")
+plt.show()
