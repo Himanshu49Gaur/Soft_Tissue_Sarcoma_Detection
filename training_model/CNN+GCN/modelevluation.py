ec690818b446ee7a324ddbe00c5cfa5308ac2984
Himanshu Gaur <161289041+Himanshu49Gaur@users.noreply.github.com>
2025-10-18 00:40:04 +0530

    Update modelevluation.py

--- training_model/CNN+GCN/modelevluation.py
+++ training_model/CNN+GCN/modelevluation.py
@@ -1,3 +1,12 @@
 # =============================================
 # 6.7–6.9: CNN + GCN Evaluation, ROC, Loss/Accuracy Plots
 # =============================================
+
+import torch
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
